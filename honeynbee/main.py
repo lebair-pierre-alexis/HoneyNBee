18d96d7fd836abc642e34f1b2492f625049cc1b7
Ulquileon <pierre-alexis.lebair@epitech.eu>
2022-10-20 23:12:53 +0200

    Fixed generation number issue

--- honeynbee/main.py
+++ honeynbee/main.py
@@ -6,7 +6,7 @@ from honeynbee.common import utils
 def run():
     pop, flowercount, gen = utils.takeInput()
     my_hive = hive.Hive(popSize= pop, mapSize= 1000, flowers= flowercount)
-    for gen in range(10000):
+    for generation in range(gen):
         my_hive.fitness()
         my_hive.rank()
         if gen == 0:
